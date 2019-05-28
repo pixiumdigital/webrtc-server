9aaaca72edd630694c7d58d93b1188839676a482
crimson-med <mederic.burlet@gmail.com>
2019-05-28 14:37:58 +0800

    fix cors

--- app.js
+++ app.js
@@ -17,7 +17,18 @@ if (process.env.LOCAL) {
 } else {
   server = http.createServer(app);
 }
-var io = require('socket.io')(server);
+//var io = require('socket.io')(server);
+const io = require('socket.io')(server, {
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Credentials": true
+        };
+        res.writeHead(200, headers);
+        res.end();
+    }
+});
 io.origins((origin, callback) => {
   return callback(null, true);
 });
